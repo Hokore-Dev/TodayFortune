8ef72af0b31e07705cdefbf7af075f5dd3e055f6
minjun.ha <haminjun0@gmail.com>
2019-02-16 16:55:53 +0900

    기본 프로젝트 구성 추가

---
+++ TodayFortune/TodayFortune WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TodayFortune WatchKit Extension
+//
+//  Created by minjun.ha on 16/02/2019.
+//  Copyright © 2019 thechamcham. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ TodayFortune/TodayFortune/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodayFortune
+//
+//  Created by minjun.ha on 16/02/2019.
+//  Copyright © 2019 thechamcham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
