e9d8c195e7085241a45bd602f6c252f9b76c2cbc
minjun.ha <haminjun0@gmail.com>
2019-02-16 21:00:07 +0900

    운세 API 연결

--- TodayFortune/TodayFortune WatchKit Extension/InterfaceController.swift
+++ TodayFortune/TodayFortune WatchKit Extension/InterfaceController.swift
@@ -16,6 +16,8 @@ class InterfaceController: WKInterfaceController {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        
+        onHttpRequest()
     }
     
     override func willActivate() {
@@ -28,4 +30,39 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    func onHttpRequest() {
+  
+        //URL생성
+        guard let url = URL(string: "http://erumyasp.azurewebsites.net/api/fortune_daily.php?tday=20190216&uday=19981026&lunar=1&monthType=1") else {return}
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "get" //get : Get 방식, post : Post 방식
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            //error 일경우 종료
+            guard error == nil && data != nil else {
+                if let err = error {
+                    print(err.localizedDescription)
+                }
+                return
+            }
+            
+            //data 가져오기
+            if let _data = data {
+                if let strData = NSString(data: _data, encoding: String.Encoding.utf8.rawValue) {
+                    let str = String(strData)
+                    //print(str)
+                    //메인쓰레드에서 출력하기 위해
+                    DispatchQueue.main.async {
+                        print(str)
+                    }
+                }
+            }else{
+                print("data nil")
+            }
+        })
+        task.resume()
+    }
 }
